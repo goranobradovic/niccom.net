2971a1cb887dc19a646a3ca66dba2f51d39eea70
Goran Obradović <gazda@goranobradovic.com>
2012-08-12 02:31:24 +0200

    first version (with ko links)

---
+++ Website/links.js
@@ -0,0 +1,55 @@
+﻿var links = {
+    groups: [
+        {
+            group: "Vijesti",
+            links: [
+                { url: "www.nezavisne.com", name: "Nezavisne" },
+                { url: "www.jutarnji.hr", name: "Jutarnji List" },
+                { url: "www.index.hr", name: "Index" },
+                { url: "www.dnevniavaz.ba", name: "Dnevni Avaz" },
+                { url: "www.klix.ba", name: "Klix" }
+            ]
+        },
+        {
+            group: "Vijesti",
+            links: [
+                { url: "www.nezavisne.com", name: "Nezavisne" },
+                { url: "www.jutarnji.hr", name: "Jutarnji List" },
+                { url: "www.klix.ba", name: "Klix" }
+            ]
+        },
+        {
+            group: "Vijesti",
+            links: [
+                { url: "www.klix.ba", name: "Klix" }
+            ]
+        },
+        {
+            group: "Vijesti",
+            links: [
+                { url: "www.nezavisne.com", name: "Nezavisne" },
+                { url: "www.jutarnji.hr", name: "Jutarnji List" },
+                { url: "www.index.hr", name: "Index" },
+                { url: "www.dnevniavaz.ba", name: "Dnevni Avaz" },
+                { url: "www.klix.ba", name: "Klix" }
+            ]
+        },
+        {
+            group: "Zabava",
+            links: [
+                { url: "www.facebook.com", name: "Facebook" },
+                { url: "www.twitter.com", name: "Twitter" },
+                { url: "plus.google.com", name: "Google plus" },
+                { url: "www.cafe.ba", name: "Cafe.ba" },
+            ]
+        },
+        {
+            group: "Banja Luka",
+            links: [
+                { url: "www.bl-bioskopi.com", name: "Bioskopi", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.bl-banke.com", name: "Banke" },
+                { url: "www.bl-hoteli.com", name: "Hoteli" },
+            ]
+        }
+    ]
+};
---
+++ Website/script/niccom.js
@@ -0,0 +1,59 @@
+﻿
+
+var Niccom = (function (Niccom) {
+    /// <summary>
+    /// part of the Niccom module
+    /// </summary>
+    var self = Niccom;
+
+    function LinkViewModel(data) {
+        var self = this;
+
+        self.url = "http://" + data.url;
+        self.name = data.name;
+        self.favicon = data.favicon || ("http://" + data.url + "/favicon.ico");
+    }
+
+    function GroupViewModel(data) {
+        var self = this;
+        self.name = data.group;
+        var links = $.map(data.links, function (item) { return new LinkViewModel(item) });
+        self.links = ko.observableArray(links);
+    }
+
+    function LinksViewModel(data) {
+        /// <summary>
+        /// Links submodule of the Niccom
+        /// </summary>
+        var self = this;
+        var groups = $.map(data.groups, function (item) { return new GroupViewModel(item) });
+        self.groups = ko.observableArray(groups);
+
+        var _refresh = function () {
+            /// <summary>
+            /// refresh method in Niccom.Links
+            /// </summary>
+        }
+        if (links) {
+        }
+        return {
+            /// Niccom.refresh.refresh()
+            refresh: _refresh
+        };
+    }
+    function adjustWidth() {
+        $("#links").width($(window).width() - 500);
+    }
+    function init() {
+        /// <summary>
+        /// This places result of Links() execution under
+        /// Niccom.
+        /// </summary>
+        self.LinksViewModel = self.LinksViewModel || LinksViewModel(links);
+        ko.applyBindings(self.LinksViewModel);
+        $(window).resize(adjustWidth);
+        adjustWidth();
+    };
+    $(document).ready(init);
+    return self;
+}(Niccom || {}));
