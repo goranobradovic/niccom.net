c8fd3dc9fa280c9d5ca31d351bdb0bee10835019
pcservis <pcsniccom@gmail.com>
2012-08-22 14:00:41 +0300

    Update Website/links.js

--- Website/links.js
+++ Website/links.js
@@ -10,56 +10,204 @@
         {
             group: "Vijesti",
             links: [
-                { url: "www.test.com", name: "Test" },
-                { url: "www.test2.com", name: "Test" }
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
             ]
         },
         {
             group: "Ženski Kutak",
             links: [
-                { url: "www.test.com", name: "Test" },
-                { url: "www.test2.com", name: "Test" }
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
             ]
         },
         {
             group: "TV Program",
             links: [
-                { url: "www.test.com", name: "Test" },
-                { url: "www.test2.com", name: "Test" }
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
             ]
         },
         {
             group: "OnLine TV",
             links: [
-                { url: "www.test.com", name: "Test" },
-                { url: "www.test2.com", name: "Test" }
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
             ]
         },
         {
             group: "OnLine Radio",
             links: [
-                { url: "www.test.com", name: "Test" },
-                { url: "www.test2.com", name: "Test" }
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
             ]
         },
         {
-            group: "Banja Luka",
+            group: "OnLine Filmovi",
             links: [
-                { url: "www.bl-bioskopi.com", name: "Bioskopi", favicon: "http://www.niccom.net/favicon.ico" },
-                { url: "www.bl-banke.com", name: "Banke" },
-                { url: "www.bl-hoteli.com", name: "Hoteli" },
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "OnLine Igre",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Sport",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Zabava",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Muzika",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Torrenti",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Tehnologija",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Društvene Mreže",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Programi",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "XXX",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Pretraživači",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Webmail",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Mali Oglasi",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Grupni Popusti",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
             ]
         },
         {
             group: "Korisno",
             links: [
-                { url: "www.pcidatabase.com", name: "PCI Database", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
             ]
         },
         {
-            group: "Online igre",
+            group: "Zdravlje i Fitness",
             links: [
-                { url: "www.yourownstrippoker.com", name: "Strip poker", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Forumi",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Vremenska Prognoza",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Banke",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Restorani i Dostave Hrane",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Hoteli",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Klubovi i Kafići",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Internet Provajderi",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
+            ]
+        },
+        {
+            group: "Tržni Centri i Prodavnice",
+            links: [
+                { url: "www.test.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" },
+                { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
             ]
         },
         {}, {}, {}, {}, {}
