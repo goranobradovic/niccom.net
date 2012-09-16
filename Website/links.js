0786498125f6eeecd55552dc7323df5563ff447b
pcservis <pcsniccom@gmail.com>
2012-09-16 16:43:56 +0300

    Update Website/links.js

--- Website/links.js
+++ Website/links.js
@@ -190,7 +190,7 @@
         {
             group: "Webmail",
             links: [
-                { url: "mail.google.com", name: "Gmail", favicon: "mail.google.com/favicon.ico" },
+                { url: "mail.google.com", name: "Gmail" },
                 { url: "www.hotmail.com", name: "Hotmail", },
                 { url: "mail.yahoo.com", name: "Yahoo! Mail", },
                 { url: "www.gmx.com", name: "GMX", },
