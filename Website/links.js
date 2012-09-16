b30c86504dbc6b44e45bb5612302e652221416c1
pcservis <pcsniccom@gmail.com>
2012-09-16 17:00:34 +0300

    Update Website/links.js

--- Website/links.js
+++ Website/links.js
@@ -29,11 +29,9 @@
             group: "Banja Luka",
             links: [
                 { url: "blmap.inecco.net", name: "Mapa Banja Luke", favicon: "http://users.teol.net/~pcservis/misc/favicon.ico" },
-                { url: "www.banjaluka.rs.ba", name: "Banja Luka" },
+                { url: "www.banjaluka.rs.ba", name: "Banja Luka", favicon: "http://users.teol.net/~pcservis/misc/favicon.ico" },
                 { url: "www.banjaluka-tourism.com", name: "TOBL" },
-                { url: "www.bl-bioskop.ba", name: "BL Bioskop" },
-                { url: "www.test2.com", name: "Test" },
-                { url: "www.test.com", name: "Test" },
+                { url: "www.bl-bioskop.ba", name: "BL Bioskop", favicon: "http://users.teol.net/~pcservis/misc/favicon.ico" },
                 { url: "www.test2.com", name: "Test", favicon: "http://www.niccom.net/favicon.ico" }
             ]
         },
